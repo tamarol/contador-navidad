5b352a2841b5f32f4de5bfb740cccb2650db20ac
Alfredo Silva <Tamarolsilva@gmail.com>
2024-11-01 16:54:37 -0600

    Commit2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true, 
+  },
+  basePath: '/contador-navidad', 
+  assetPrefix: '/contador-navidad', 
 };
 
 export default nextConfig;
